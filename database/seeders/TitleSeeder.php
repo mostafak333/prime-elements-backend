58c7863084945c214c868fd9aead9ac9fd4f7683
Mostafa Kamal <mostafakamalhassan0@gmail.com>
2026-08-17 22:45:56 +0300

    add seeder

---
+++ database/seeders/TitleSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Title;
+
+class TitleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $titles = [
+            [
+                'name_en' => 'School Books',
+                'name_ar' => 'الكتب المدرسية',
+            ],
+            [
+                'name_en' => 'University Books',
+                'name_ar' => 'الكتب الجامعية',
+            ],
+            [
+                'name_en' => 'Academic Books',
+                'name_ar' => 'الكتب الأكاديمية',
+            ],
+            [
+                'name_en' => 'Fiction Books',
+                'name_ar' => 'الكتب الخيالية',
+            ],
+        ];
+
+        foreach ($titles as $title) {
+            Title::create([
+                'name_en' => $title['name_en'],
+                'name_ar' => $title['name_ar'],
+                'created_by' => 1,
+                'updated_by' => 1,
+            ]);
+        }
+    }
+}
